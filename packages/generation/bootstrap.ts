63594f7ef2ce3c67335f8b43783878140d0790a9
Kirill Bondarenko <billionmann@gmail.com>
2024-06-06 17:28:25 +1000

    wip: testing redirect to the Nexton App #1

--- packages/generation/bootstrap.ts
+++ packages/generation/bootstrap.ts
@@ -1,21 +1,21 @@
-import '@tok/ui/styles/global.scss';
+import "@tok/ui/styles/global.scss";
 
-import { DefinePresetsPlugin } from '@tok/generation/plugins/definePresets';
-import { FormStatePlugin } from '@tok/generation/plugins/formState';
-import { ThemePlugin } from '@tok/generation/plugins/theme';
-import { TokI18nPlugin } from '@tok/i18n';
-import { useTelegramSdk } from '@tok/telegram-ui/use/sdk';
-import { AlertsPlugin } from '@tok/ui/plugins/alerts';
-import { CurrencyPlugin } from '@tok/ui/plugins/currency';
-import { createApp, defineComponent } from 'vue';
+import { DefinePresetsPlugin } from "@tok/generation/plugins/definePresets";
+import { FormStatePlugin } from "@tok/generation/plugins/formState";
+import { ThemePlugin } from "@tok/generation/plugins/theme";
+import { TokI18nPlugin } from "@tok/i18n";
+import { useTelegramSdk } from "@tok/telegram-ui/use/sdk";
+import { AlertsPlugin } from "@tok/ui/plugins/alerts";
+import { CurrencyPlugin } from "@tok/ui/plugins/currency";
+import { createApp, defineComponent } from "vue";
 import {
   createRouter,
   createWebHistory,
   RouteLocationNormalized,
   RouteRecordRaw,
-} from 'vue-router';
+} from "vue-router";
 
-import { BootstrapConfig } from './defineConfig';
+import { BootstrapConfig } from "./defineConfig";
 
 type _App = Parameters<typeof createApp>[0];
 
@@ -27,14 +27,14 @@ export function bootstrap<T extends BootstrapConfig<any>>(
   const tg = useTelegramSdk();
 
   const i18nOptions = {
-    default: fallback || 'en',
+    default: fallback || "en",
     asyncLocales,
   };
 
   const pages = config.pages.map((config, index) => {
     return {
-      path: index === 0 ? '/' : config.path || `/${index}`,
-      component: () => import('@tok/generation/presets/Route.vue'),
+      path: index === 0 ? "/" : config.path || `/${index}`,
+      component: () => import("@tok/generation/presets/Route.vue"),
       meta: {
         config,
       },
@@ -42,15 +42,17 @@ export function bootstrap<T extends BootstrapConfig<any>>(
   });
 
   const exitRoute: RouteRecordRaw = {
-    path: '/exit',
+    path: "/exit",
     component: defineComponent({
-      template: '<div></div>',
+      template: "<div></div>",
       beforeRouteEnter(to, from, next) {
         if (tg) {
-          console.log('Running inside Telegram Mini App - closing');
-          tg.close();
+          console.log("Running inside Telegram Mini App - closing");
+          // tg.close();
+
+          window.location.href = "https://t.me/Nexton_tele_bot/nexton/startapp";
         } else {
-          console.log('Not running inside Telegram Mini App - cannot close');
+          console.log("Not running inside Telegram Mini App - cannot close");
           next(false); // Cancel navigation if not in Telegram Mini App
         }
       },
@@ -74,11 +76,11 @@ export function bootstrap<T extends BootstrapConfig<any>>(
   }; */
 
   const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL || '/'),
+    history: createWebHistory(import.meta.env.BASE_URL || "/"),
     routes: ([] as RouteRecordRaw[]).concat(pages).concat(exitRoute).concat({
-      path: '/not-found',
-      alias: '/:catchAll(.*)*',
-      redirect: '/',
+      path: "/not-found",
+      alias: "/:catchAll(.*)*",
+      redirect: "/",
     }),
   });
 
@@ -92,10 +94,10 @@ export function bootstrap<T extends BootstrapConfig<any>>(
 
   router.onError((error, to) => {
     const dynamicallyImportsError =
-      error.message.includes('Failed to fetch dynamically imported module') ||
-      error.message.includes('Importing a module script failed');
+      error.message.includes("Failed to fetch dynamically imported module") ||
+      error.message.includes("Importing a module script failed");
 
-    if (error.name === 'ChunkLoadError' || dynamicallyImportsError) {
+    if (error.name === "ChunkLoadError" || dynamicallyImportsError) {
       location.href = to.fullPath;
     }
   });
@@ -104,9 +106,9 @@ export function bootstrap<T extends BootstrapConfig<any>>(
     .use(router)
     .use(AlertsPlugin)
     .use(TokI18nPlugin, i18nOptions)
-    .use(ThemePlugin, config.theme || 'auto')
+    .use(ThemePlugin, config.theme || "auto")
     .use(FormStatePlugin)
     .use(DefinePresetsPlugin, config.definePresets || {})
     .use(CurrencyPlugin, config.currencyConfig || {})
-    .mount('#app');
+    .mount("#app");
 }
